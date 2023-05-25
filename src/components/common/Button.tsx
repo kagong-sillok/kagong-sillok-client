47775c9dc9d6ab7114ee249e1e61a135ec0a9a74
kang <kangju2000@naver.com>
2023-05-25 15:21:07 +0900

    feat: Button 컴포넌트 구현

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,30 @@
+interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+  type: 'Default' | 'Round_Default';
+  children: React.ReactNode;
+  className?: string;
+  disabled?: boolean;
+}
+export default function Button({
+  type,
+  children,
+  className,
+  disabled = false,
+  ...props
+}: ButtonProps) {
+  const buttonStyle = {
+    Default:
+      'bg-black py-4 text-button1 text-white active:bg-opacity-80 disabled:bg-opacity-10 disabled:text-black',
+    Round_Default:
+      'rounded-full border border-black bg-white py-[11px] text-button2 text-black active:bg-opacity-10 disabled:bg-opacity-20 disabled:border-opacity-20',
+  }[type];
+
+  return (
+    <button
+      className={`w-full disabled:text-opacity-40 ${buttonStyle} ${className ?? ''}`}
+      disabled={disabled}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
