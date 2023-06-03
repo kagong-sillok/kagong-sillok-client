82b5a92ae1ceeb4646ddcc5e829dd68b8c86aef1
YunjiNam <yj607252@gmail.com>
2023-06-03 21:17:23 +0900

    feat: 각 페이지 ui 추가

--- src/app/mypage/place/bookmarks/page.tsx
+++ src/app/mypage/place/bookmarks/page.tsx
@@ -1,5 +1,56 @@
+import { BookmarkItem } from '@/components/mypage';
+import { PlaceShortData } from '@/types/place';
+
+const bookmarkList: PlaceShortData = {
+  data: {
+    places: [
+      {
+        id: 1,
+        name: '스타벅스 동대문공원점',
+        tags: ['#조용한', '#나만알고싶은', '#노트북'],
+        rating: 3,
+        latitude: 123.123,
+        longitude: 123.123,
+        isOpen: true,
+        createdAt: '',
+        updatedAt: '',
+      },
+      {
+        id: 2,
+        name: '스타벅스 동대문공원점',
+        tags: ['#노트북'],
+        rating: 2,
+        latitude: 123.123,
+        longitude: 123.123,
+        isOpen: true,
+        createdAt: '',
+        updatedAt: '',
+      },
+      {
+        id: 3,
+        name: '스타벅스 동대문공원점',
+        tags: ['#조용한', '#나만알고싶은'],
+        rating: 4,
+        latitude: 123.123,
+        longitude: 123.123,
+        isOpen: true,
+        createdAt: '',
+        updatedAt: '',
+      },
+    ],
+  },
+};
+
 function Bookmarks() {
-  return <div>Bookmarks</div>;
+  return (
+    <>
+      {bookmarkList.data.places.map((item, i) => {
+        return (
+          <BookmarkItem key={i} data={item} isLast={bookmarkList.data.places.length - 1 === i} />
+        );
+      })}
+    </>
+  );
 }
 
 export default Bookmarks;
--- src/app/mypage/record/timeline/page.tsx
+++ src/app/mypage/record/timeline/page.tsx
@@ -1,5 +1,41 @@
+import RecordItem from '@/components/mypage/RecordItem';
+import { RecordData } from '@/types/mypage';
+
+const recordList: RecordData = {
+  data: [
+    {
+      id: 1,
+      name: '스타벅스 동대문공원점',
+      content: '모각코',
+      latitude: 123.123,
+      longitude: 123.123,
+      image: 'imageUrl',
+      createdAt: '2023-06-01T00:04:00',
+      updatedAt: '2023-06-01T00:00:00',
+      time: '2023-06-01T00:04:00',
+    },
+    {
+      id: 2,
+      name: '스타벅스 동대문공원점',
+      content: '모각코',
+      latitude: 123.123,
+      longitude: 123.123,
+      createdAt: '2023-06-01T01:20:00',
+      image: 'imageUrl',
+      updatedAt: '2023-06-01T00:00:00',
+      time: '2023-06-01T01:20:00',
+    },
+  ],
+};
+
 function Timeline() {
-  return <div>Timeline</div>;
+  return (
+    <div className="flex w-full flex-col items-center justify-center gap-6 p-6">
+      {recordList.data.map((item, i) => {
+        return <RecordItem key={i} data={item} />;
+      })}
+    </div>
+  );
 }
 
 export default Timeline;
