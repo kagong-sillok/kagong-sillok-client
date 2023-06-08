c2e329b142603d29664049cab30af94fe84bcd87
YunjiNam <yj607252@gmail.com>
2023-06-08 15:02:04 +0900

    feat: 주 달력 추가

---
+++ src/components/mypage/Weekly.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import { format, startOfWeek } from 'date-fns';
+import Image from 'next/image';
+
+import type { CalendarType } from '@/types/mypage';
+
+const koDays = ['일', '월', '화', '수', '목', '금', '토'];
+
+function Weekly({ onViewChange }: { onViewChange: (type: CalendarType) => void }) {
+  const today = new Date();
+  const year = Number(format(startOfWeek(today), 'yyyy'));
+  const month = Number(format(startOfWeek(today), 'MM'));
+  const start = Number(format(startOfWeek(today), 'dd'));
+  const days = [...new Array(7)].map((_, i) => i + start);
+
+  return (
+    <div className="relative flex h-40 w-full flex-col items-center justify-center gap-[15px] bg-violet/default px-6 py-5">
+      <div
+        className="absolute right-6 top-6 cursor-pointer text-caption text-background"
+        onClick={() => onViewChange('MONTH')}
+      >
+        달력 펼치기
+      </div>
+      <div
+        className="flex cursor-pointer gap-0.5 text-sub1 text-background"
+        onClick={() => onViewChange('MONTH')}
+      >
+        {year}년 {month}월
+        <Image
+          src={`/assets/icons/16/Arrow-down.svg`}
+          alt={`Arrow-down`}
+          width={16}
+          height={16}
+          className="invert filter"
+        />
+      </div>
+      <div className="relative flex w-full items-center justify-between">
+        {koDays.map((el) => {
+          return (
+            <div key={el} className="h-5 w-[45px] text-center text-caption text-background">
+              {el}
+            </div>
+          );
+        })}
+      </div>
+      <div className="relative flex w-full items-center justify-between">
+        {days.map((el) => {
+          return (
+            <div
+              key={el}
+              className="flex h-[45px] w-11 flex-col items-center justify-center gap-[5px] text-caption text-background"
+            >
+              <Image
+                src="/assets/icons/24/calendar_off.svg"
+                alt="calendar_off"
+                width={24}
+                height={24}
+              />
+              <div>{el}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Weekly;
