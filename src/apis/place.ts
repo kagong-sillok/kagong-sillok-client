d8213333e8d2a7540ac77016482879cae64c433b
kang <kangju2000@naver.com>
2023-06-09 19:37:45 +0900

    refactor: console.log 제거

--- src/apis/place.ts
+++ src/apis/place.ts
@@ -1,9 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import ky from 'ky';
 
 import type { ImageType, PlaceType, ReviewType } from '@/types/place';
 
 export const getPlace = async (id: string) => {
-  console.log('getPlace: ', id);
   const data = await ky.get(`http://3.37.38.169:8080/api/v1/places/${id}`);
   // const data = await ky.get('/db/place.json');
   const json = await data.json<{ data: PlaceType }>();
@@ -12,7 +12,6 @@ export const getPlace = async (id: string) => {
 };
 
 export const getReviews = async (placeId: string, size: number, pageParam: number) => {
-  console.log(`/api/v1/reviews/search?page=${pageParam}&size=${size}&placeId=${placeId}`);
   const data = await ky.get('/db/reviews.json');
   const json = await data.json<{ data: { reviews: ReviewType[] } }>();
 
@@ -23,7 +22,6 @@ export const getReviews = async (placeId: string, size: number, pageParam: numbe
 };
 
 export const getImages = async (imageIds: number[]) => {
-  console.log('getImages: ', imageIds);
   const data = await ky.get('http://3.37.38.169:8080/api/v1/images', {
     searchParams: {
       imageIds: imageIds.join(','),
