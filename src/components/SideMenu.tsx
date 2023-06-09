c7b992b5bb8abcb75ae3895ec9cb1bba73581bcd
YunjiNam <yj607252@gmail.com>
2023-06-09 14:02:39 +0900

    refactor: 컬러 수정

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -36,7 +36,7 @@ export default function SideMenu({ open, onClose }: SideMenuProps) {
   if (!open) return <></>;
   return (
     <div className="absolute left-0 top-0 z-50 flex h-full w-full items-start justify-start">
-      <div className="h-full w-72 bg-background px-6 py-5">
+      <div className="h-full w-72 bg-white px-6 py-5">
         <div className="mt-5 flex flex-col items-center justify-center gap-5">
           <div className="flex w-full items-center justify-start gap-3">
             <div className="h-[52px] w-[52px] rounded-full bg-bk40" />
