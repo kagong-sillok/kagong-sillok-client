95c71e326773ec2d73c8e17d21015875d0192ff3
kang <kangju2000@naver.com>
2023-06-09 14:20:45 +0900

    refactor: KakaoButton 컴포넌트에 HTMLButtonElement 프로퍼티 추가

--- src/components/auth/KakaoButton.tsx
+++ src/components/auth/KakaoButton.tsx
@@ -1,15 +1,16 @@
 import Image from 'next/image';
 
-interface KakaoButtonProps {
+interface KakaoButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   onClick?: () => void;
   className?: string;
 }
 
-export default function KakaoButton({ onClick, className }: KakaoButtonProps) {
+export default function KakaoButton({ onClick, className, ...props }: KakaoButtonProps) {
   return (
     <button
-      className={`flex w-full items-center justify-center bg-kakao py-3 ${className ?? ''}`}
+      className={`flex w-full items-center justify-center bg-kakao py-3 ${className ?? ''}}`}
       onClick={onClick}
+      {...props}
     >
       <Image src="/assets/Icons/kakaoLogin.svg" alt="kakao-login" width={17} height={17} />
       <p className="ml-3 text-[15px] font-semibold">카카오 로그인</p>
