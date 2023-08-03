7a98a0d8565bceaa2ce6acae4b8e84d8039b5986
kang <kangju2000@naver.com>
2023-08-03 17:22:41 +0900

    refactor: PlaceTopNavigationBar 리팩토링

--- src/app/place/components/PlaceTopNavigationBar/index.tsx
+++ src/app/place/components/PlaceTopNavigationBar/index.tsx
@@ -1,8 +1,9 @@
 'use client';
 import { MAP_HEIGHT } from '../../constants';
-import Header from '../Header';
 import ShareSheet from '../ShareSheet';
+import TopNavigationBar from '@/components/TopNavigationBar';
 import { useDetectScroll } from '@/hooks/useDetectScroll';
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
@@ -20,26 +21,29 @@ export default function PlaceTopNavigationBar({ name }: PlaceTopNavigationBarPro
 
   return (
     <>
-      <Header
-        name={isScrolled ? name : ''}
+      <TopNavigationBar
+        title={isScrolled ? name : ''}
         className={isScrolled ? '' : 'bg-opacity-0 invert filter'}
         onBackClick={() => router.push('/')}
-        rightIcons={[
-          {
-            src: '/assets/icons/28/Bookmark.svg',
-            alt: 'Bookmark',
-            width: 28,
-            height: 28,
-            onClick: () => console.log('북마크 클릭'),
-          },
-          {
-            src: '/assets/icons/28/Share.svg',
-            alt: 'Share',
-            width: 28,
-            height: 28,
-            onClick: () => setIsShareSheetOpen(true),
-          },
-        ]}
+        rightNode={
+          <>
+            <Image
+              src="/assets/icons/28/Bookmark.svg"
+              alt="Bookmark"
+              width={28}
+              height={28}
+              onClick={() => console.log('북마크 클릭')}
+            />
+            <Image
+              src="/assets/icons/28/Share.svg"
+              alt="Share"
+              width={28}
+              height={28}
+              onClick={() => setIsShareSheetOpen(true)}
+            />
+          </>
+        }
+        isSpacing={false}
       />
       <ShareSheet isOpen={isShareSheetOpen} onClose={() => setIsShareSheetOpen(false)} />
     </>
