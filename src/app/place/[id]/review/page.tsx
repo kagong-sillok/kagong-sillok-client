e13b21280424dcd3f373ede475a93bb6971b430b
kang <kangju2000@naver.com>
2023-08-03 17:25:49 +0900

    refactor: TopNavigationBar 적용

--- src/app/place/[id]/review/page.tsx
+++ src/app/place/[id]/review/page.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import { useGetReviews } from '@/apis/review';
-import { Header, ReviewItem } from '@/app/place/components';
+import { ReviewItem } from '@/app/place/components';
+import TopNavigationBar from '@/components/TopNavigationBar';
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
 export default function Page({ params }: { params: { id: string } }) {
@@ -12,25 +14,25 @@ export default function Page({ params }: { params: { id: string } }) {
   const { data: reviewsData } = useGetReviews(placeId);
 
   return (
-    <div className="pt-14">
-      <Header
-        name="리뷰더보기"
+    <main>
+      <TopNavigationBar
+        title="리뷰더보기"
         onBackClick={() => router.push(`/place/${placeId}`)}
-        rightIcons={[
-          {
-            src: '/assets/icons/28/Close.svg',
-            alt: 'Close',
-            width: 28,
-            height: 28,
-            onClick: () => router.push(`/place/${placeId}`),
-          },
-        ]}
+        rightNode={
+          <Image
+            src="/assets/icons/28/Close.svg"
+            alt="Close"
+            width={28}
+            height={28}
+            onClick={() => router.push(`/place/${placeId}`)}
+          />
+        }
       />
       <div className="my-6 flex flex-col gap-5 px-6">
         {reviewsData?.pages.map(({ data }) =>
           data.reviews.map((review) => <ReviewItem key={review.id} review={review} />)
         )}
       </div>
-    </div>
+    </main>
   );
 }
