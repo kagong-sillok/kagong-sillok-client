e666d14c298dfbebe2a263b4bff68749d0c4a9d6
kang <kangju2000@naver.com>
2023-08-05 21:20:59 +0900

    refactor: ReviewItem -> ReviewList로 변경

--- src/app/place/components/ReviewList/index.tsx
+++ src/app/place/components/ReviewList/index.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { useGetImages } from '@/apis/image';
 import { RATING_TEXT } from '@/app/place/constants';
 import { format } from 'date-fns';
@@ -5,12 +6,25 @@ import Image from 'next/image';
 
 import type { Review } from '@/types/review';
 
-interface ReviewItemProps {
-  review: Review;
+interface ReviewListProps {
+  reviewsData: Review[];
 }
 
-export default function ReviewItem({ review }: ReviewItemProps) {
-  const { data: imagesData } = useGetImages(review.imageIds || []);
+export default function ReviewList({ reviewsData }: ReviewListProps) {
+  return (
+    <div className="flex flex-col gap-5">
+      {reviewsData?.map((review) => <ReviewItem key={review.id} reviewData={review} />)}
+    </div>
+  );
+}
+
+interface ReviewItemProps {
+  reviewData: Review;
+}
+
+function ReviewItem({ reviewData }: ReviewItemProps) {
+  const { imageIds, content, memberNickname, rating, writtenAt } = reviewData;
+  const { data: imagesData } = useGetImages(imageIds);
 
   return (
     <div className="flex gap-3.5">
@@ -18,20 +32,20 @@ export default function ReviewItem({ review }: ReviewItemProps) {
       <div className="flex flex-grow flex-col overflow-hidden">
         <div className="mb-[13px] flex h-10 justify-between">
           <div>
-            <p className="text-body2">{review.userNickname}</p>
+            <p className="text-body2">{memberNickname}</p>
             <div className="flex items-center">
               <Image
-                src={`/assets/icons/40/emoji-rating${review.rating}_on.svg`}
+                src={`/assets/icons/40/emoji-rating${rating}_on.svg`}
                 alt="emoji"
                 width={16}
                 height={16}
               />
               <p className="circle relative pl-2.5 text-[12px] font-normal text-bk40">
-                {RATING_TEXT[review.rating - 1]}
+                {RATING_TEXT[rating - 1]}
               </p>
             </div>
           </div>
-          <p className="text-caption text-bk40">{format(new Date(review.createdAt), 'yy.MM.dd')}</p>
+          <p className="text-caption text-bk40">{format(new Date(writtenAt), 'yy.MM.dd')}</p>
         </div>
         <div
           className={`mb-2.5 flex items-center gap-0.5 overflow-x-scroll ${
@@ -50,7 +64,7 @@ export default function ReviewItem({ review }: ReviewItemProps) {
             </div>
           ))}
         </div>
-        <p className="text-body2">{review.content}</p>
+        <p className="text-body2">{content}</p>
       </div>
     </div>
   );
