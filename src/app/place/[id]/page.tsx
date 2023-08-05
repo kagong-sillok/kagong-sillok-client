df38cfbc5bf34ff304aea9a9b383a12e75b5f77a
kang <kangju2000@naver.com>
2023-08-05 21:33:56 +0900

    refactor: QueryAsuncBoundary로 에러 처리

--- src/app/place/[id]/page.tsx
+++ src/app/place/[id]/page.tsx
@@ -1,16 +1,25 @@
-import PlaceDetail from './components/PlaceDetail';
-import { Keys, getPlace } from '@/apis/place';
+import { PlaceDetail } from './components';
+import { Keys as PlaceKeys, getPlace } from '@/apis/place';
+import { Keys as ReviewKeys, getReviews } from '@/apis/review';
 import { HydrationProvider } from '@/providers/HydrationProvider';
-import { Suspense } from '@suspensive/react';
+import { QueryAsyncBoundary } from '@suspensive/react-query';
 
 export default function PlacePage({ params }: { params: { id: string } }) {
   const placeId = Number(params.id);
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <HydrationProvider queryKey={Keys.place(placeId)} queryFn={() => getPlace(placeId)}>
-        <PlaceDetail placeId={placeId} />
+    <QueryAsyncBoundary
+      rejectedFallback={<div>에러가 발생했습니다.</div>}
+      pendingFallback={<div>로딩중...</div>}
+    >
+      <HydrationProvider queryKey={PlaceKeys.place(placeId)} queryFn={() => getPlace(placeId)}>
+        <HydrationProvider
+          queryKey={ReviewKeys.reviews(placeId)}
+          queryFn={() => getReviews(placeId)}
+        >
+          <PlaceDetail placeId={placeId} />
+        </HydrationProvider>
       </HydrationProvider>
-    </Suspense>
+    </QueryAsyncBoundary>
   );
 }
