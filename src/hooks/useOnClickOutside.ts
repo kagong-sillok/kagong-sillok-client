e532f324fd1aac61914a79220e8cf21ad11c0d7a
kang <kangju2000@naver.com>
2023-08-15 03:30:53 +0900

    refactor: useOnClickOutside hook 마우스를 놓을 때 실행되도록 수정

--- src/hooks/useOnClickOutside.ts
+++ src/hooks/useOnClickOutside.ts
@@ -13,11 +13,11 @@ export function useOnClickOutside<T extends HTMLElement = HTMLElement>(
       handler(event);
     };
 
-    document.addEventListener('mousedown', listener);
-    document.addEventListener('touchstart', listener);
+    document.addEventListener('mouseup', listener);
+    document.addEventListener('touchend', listener);
     return () => {
-      document.removeEventListener('mousedown', listener);
-      document.removeEventListener('touchstart', listener);
+      document.removeEventListener('mouseup', listener);
+      document.removeEventListener('touchend', listener);
     };
   }, [ref, handler]);
 }
