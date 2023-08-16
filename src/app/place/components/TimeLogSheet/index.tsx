eff66c77c05eac118355f71542d8fa7c01a993bb
kang <kangju2000@naver.com>
2023-08-17 03:43:42 +0900

    feat: 카공기록 api 연결

--- src/app/place/components/TimeLogSheet/index.tsx
+++ src/app/place/components/TimeLogSheet/index.tsx
@@ -1,5 +1,9 @@
 'use client';
-import { Button, ImageUpload, BottomSheet } from '@/components';
+import { useImagesUpload } from '@/apis/image';
+import { usePostRecord } from '@/apis/record';
+import { useGetUserInfo } from '@/apis/user';
+import { Button, ImageUpload, BottomSheet, Spacing, Modal } from '@/components';
+import { getDate, getMonth, getYear } from 'date-fns';
 import Link from 'next/link';
 import { useRef, useState } from 'react';
 
@@ -7,18 +11,61 @@ import type { SheetRef } from 'react-modal-sheet';
 
 interface TimeLogSheetProps {
   isOpen: boolean;
+  placeId: number;
   onClose: () => void;
 }
 
-export default function TimeLogSheet({ isOpen, onClose }: TimeLogSheetProps) {
+export default function TimeLogSheet({ isOpen, placeId, onClose }: TimeLogSheetProps) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [images, setImages] = useState<File[]>([]);
-  const [text, setText] = useState('');
+  const [description, setDescription] = useState('');
 
   const ref = useRef<SheetRef>();
 
+  const { mutateAsync: uploadImagesMutateAsync } = useImagesUpload();
+  const { data: userInfoData } = useGetUserInfo({});
+  const { mutate } = usePostRecord();
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.value.length > 10) e.target.value = e.target.value.slice(0, 10);
-    setText(e.target.value);
+    setDescription(e.target.value);
+  };
+
+  const handleTimeLogSubmit = async () => {
+    if (!userInfoData?.id) throw new Error('로그인이 필요합니다.');
+
+    const date = new Date();
+    const imageIds = !!images.length
+      ? await uploadImagesMutateAsync({
+          files: images,
+          folderName: 'review',
+        }).then((res) => res.images.map((image) => image.id))
+      : [];
+
+    mutate(
+      {
+        memberId: userInfoData.id,
+        placeId,
+        studyDay: getDate(date),
+        studyMonth: getMonth(date),
+        studyYear: getYear(date),
+        description,
+        imageIds,
+        duration: 80,
+      },
+      {
+        onSuccess: () => {
+          setIsModalOpen(true);
+        },
+      }
+    );
+  };
+
+  const handleModalClose = () => {
+    onClose();
+    setIsModalOpen(false);
+    setImages([]);
+    setDescription('');
   };
 
   return (
@@ -32,16 +79,18 @@ export default function TimeLogSheet({ isOpen, onClose }: TimeLogSheetProps) {
         isBackDrop={true}
       >
         <div className="h-full px-6 pb-20 pt-8">
-          <h3 className="mb-8 text-head3">
+          <h3 className="text-head3">
             스타벅스 동대문점에서의
             <br />
             카공을 기록해 보세요!
           </h3>
+          <Spacing size={32} />
           <ImageUpload images={images} onUpload={(imageFiles) => setImages(imageFiles)} />
-
-          <hr className="my-6 text-bk10" />
-
-          <p className="mb-4 text-center text-body2 text-bk60">카공을 얼마나 했나요?</p>
+          <Spacing size={24} />
+          <hr className="text-bk10" />
+          <Spacing size={24} />
+          <p className="text-center text-body2 text-bk60">카공을 얼마나 했나요?</p>
+          <Spacing size={16} />
           <div className="mx-auto flex h-28 w-28 items-center justify-between text-head2">
             <div className="flex flex-col items-center">
               <p>00</p>
@@ -55,28 +104,41 @@ export default function TimeLogSheet({ isOpen, onClose }: TimeLogSheetProps) {
               <p>00</p>
             </div>
           </div>
-
-          <hr className="mb-6 mt-8 text-bk10" />
-
-          <p className="mb-4 text-center text-body2 text-bk60">어떤걸 했나요?</p>
+          <Spacing size={32} />
+          <hr className="text-bk10" />
+          <Spacing size={24} />
+          <p className="text-center text-body2 text-bk60">어떤걸 했나요?</p>
+          <Spacing size={16} />
           <input
             type="text"
             placeholder="오늘 한 공부를 입력해 주세요. (최대10자)"
             alt="공부내용"
             maxLength={10}
             onChange={handleChange}
-            className="mb-10 w-full bg-background p-4 text-body2 outline-none"
+            className="w-full bg-background p-4 text-body2 outline-none"
           />
+          <Spacing size={40} />
           <Link
-            href="/"
+            href={`/place/${placeId}`}
             className="block text-center text-[14px] font-normal leading-6 text-bk60 underline underline-offset-2"
           >
             리뷰도 쓰러가기
           </Link>
         </div>
       </BottomSheet>
+      <Modal isOpen={isModalOpen}>
+        <Modal.Content>카공 기록을 등록했어요!</Modal.Content>
+        <Modal.Footer>
+          <Button onClick={handleModalClose}>확인</Button>
+        </Modal.Footer>
+      </Modal>
+
       {isOpen && (
-        <Button className="fixed inset-x-0 bottom-0 z-[60] mx-auto w-full min-w-[360px] max-w-[448px]">
+        <Button
+          type="submit"
+          className="fixed inset-x-0 bottom-0 z-[60] mx-auto w-full min-w-[360px] max-w-[448px]"
+          onClick={handleTimeLogSubmit}
+        >
           카공 기록등록
         </Button>
       )}
