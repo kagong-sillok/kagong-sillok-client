fb8c3299f3e0ad8688240bd2454aba69b65d6c30
kang <kangju2000@naver.com>
2023-08-19 23:10:06 +0900

    fix: 로그인 성공 시 쿠키 추가

--- src/app/auth/kakao/redirect/page.tsx
+++ src/app/auth/kakao/redirect/page.tsx
@@ -1,4 +1,5 @@
 import { postLogin } from '@/apis/auth';
+import { setCookie } from 'cookies-next';
 import { redirect } from 'next/navigation';
 
 export default async function Redirect({
@@ -11,8 +12,15 @@ export default async function Redirect({
   await postLogin({
     authorizationCode: searchParams?.code,
     redirectUri: process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI as string,
-  }).then(() => {
+  }).then((res) => {
+    setCookie('accessToken', res.accessToken, {
+      expires: new Date(res.accessTokenExpireDateTime),
+    });
+    setCookie('refreshToken', res.refreshToken, {
+      expires: new Date(res.refreshTokenExpireDateTime),
+    });
     redirect('/');
   });
-  return <p>{searchParams?.code}</p>;
+
+  return <></>;
 }
