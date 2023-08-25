905bae968ad60d8f18aa90b727c0d9848b2f2fb4
kang <kangju2000@naver.com>
2023-08-25 13:20:33 +0900

    feat: 404 페이지 구현

---
+++ src/app/not-found.tsx
@@ -0,0 +1,30 @@
+import { Button, Spacing } from '@/components';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function NotFoundPage() {
+  return (
+    <main>
+      <header className="fixed top-0 z-20 flex h-14 w-full min-w-[360px] max-w-[448px] justify-end py-[14px] pr-6">
+        <Link href="/">
+          <Image src="/assets/icons/28/Close.svg" alt="close" width={28} height={28} />
+        </Link>
+      </header>
+      <div className="flex h-screen flex-col justify-center px-6">
+        <h3 className="text-head3">오류가 발생했어요!</h3>
+        <Spacing size={8} />
+        <p className="text-caption text-bk40">
+          존재하지 않는 주소를 입력하셨거나, <br />
+          요청하신 페이지의 주소가 변경, 삭제되어 찾을 수 없습니다.
+        </p>
+        <Spacing size={70} />
+        <Image src="/assets/icons/404.svg" alt="404" width={320} height={250} className="mx-auto" />
+        <Link href="/">
+          <Button className="fixed inset-x-0 bottom-0 z-30 mx-auto w-full min-w-[360px] max-w-[448px]">
+            홈으로 돌아가기
+          </Button>
+        </Link>
+      </div>
+    </main>
+  );
+}
